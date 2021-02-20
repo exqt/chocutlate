a3964abcd7d2641733e4ae892e5fd4027dcc480c
exqt <exqt_@naver.com>
2021-02-20 15:15:15 +0900

    Add debug libs

--- main.lua
+++ main.lua
@@ -1,19 +1,27 @@
 require 'global'
 
 local Scene = require 'scene'
-local scene = Scene()
+scene = Scene()
 local RectObject = require 'rect-object'
 local Chocolate = require 'chocolate'
 scene:addObjecct(Chocolate(0, 0, 6, 9, os.time()))
 
-function love.load()
-
+function love.load(args)
+  for k, v in ipairs(args) do
+    if v == "-debug" then
+      DEBUG = true
+    end
+  end
 end
 
 function love.mousepressed(x, y, btn) input:mousepressed(x, y, btn) end
 function love.mousereleased(x, y, btn) input:mousereleased(x, y, btn) end
 
 function love.update(dt)
+  if DEBUG then
+    require('lib.lovebird').update()
+    require('lib.lurker').update()
+  end
   scene:update(dt)
   input:clear()
 end
